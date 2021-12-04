90babd5cf1ed2b45442af819a7b4250e7073d02b
sangeeetha_sacumen <94803704+sangeetha-sacumen@users.noreply.github.com>
2021-12-04 14:31:19 +0530

    Update calculator.py

--- assignment/calculator.py
+++ assignment/calculator.py
@@ -21,14 +21,14 @@ operations = {
 }
 def switcher(choice):
    return operations.get(choice)(num1, num2)
-# print('''you can perform opeartion
-# 1.addition
-# 2.subtraction
-# 3.multiplication
-# 4.division
-# 5.module ''')
-# choice= (input("select choice from (add,sub,mul,div): "))
-# num1= int(input("enter first number: "))
-# num2= int(input("enter second number: "))
-# print(switcher(choice))
+print('''you can perform opeartion
+1.addition
+2.subtraction
+3.multiplication
+4.division
+5.module ''')
+choice= (input("select choice from (add,sub,mul,div): "))
+num1= int(input("enter first number: "))
+num2= int(input("enter second number: "))
+print(switcher(choice))
 
